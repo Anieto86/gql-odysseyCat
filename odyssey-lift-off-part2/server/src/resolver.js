85b14421bd46ec561b4ddb79a8fe798a5e1fe831
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2021-08-10 10:17:28 +0930

    resolver"

---
+++ odyssey-lift-off-part2/server/src/resolver.js
@@ -0,0 +1,11 @@
+// A resolver's mission is to populate the data for a field in your schema
+// It can fetch data from any data source, then transforms that data into the shape your client requires.
+
+const resolvers = {
+  Query: {
+    tracksForHome: (parent, arg, context, info) => {},
+  },
+};
+module.exports = resolvers;
+
+// kmlksdmflksmdflksdmflk
